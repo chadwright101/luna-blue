6669951ca0a6a6efbc8ce1c9e584100999a484c9
Chad Wright <justdeejays@gmail.com>
2023-04-21 12:50:06 +0200

    chore: add default global styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="p-4 bg-beige">
+      <h1>Hello</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero commodi
+        consequuntur hic amet, magni eius autem culpa beatae doloribus accusamus
+        reprehenderit tempora dolore veritatis officia, odio blanditiis nobis
+        distinctio, minus animi suscipit quasi quas unde ab. Earum hic saepe
+        ratione.
+      </p>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,26 @@ module.exports = {
       Comorant: "Comorant",
       Josefin_Sans: "Josefin_Sans",
     },
+    fontSize: {
+      "60px": "3.75rem",
+      "43px": "2.688rem",
+      "35px": "2.188rem",
+      "25px": "1.563rem",
+      "17px": "1.063rem",
+    },
+    colors: {
+      beige: "#F6F3EA",
+      lightBeige: "#FBFBF9",
+      darkBeige: "#F3ECE7",
+      darkGrey: "#4d4d4d",
+      black: "#0e0e0e",
+      white: "#fff",
+    },
+    fontWeight: {
+      300: "300",
+      500: "500",
+      700: "700",
+    },
 
     extend: {},
   },

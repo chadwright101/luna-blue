63a86e8e255df2006e96bbecbe83a3a68afc244f
Chad Wright <justdeejays@gmail.com>
2023-04-25 21:54:21 +0200

    feat: add image-card component & restructure our-homes page

--- components/home-page/our-rooms.tsx
+++ components/home-page/our-rooms.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Button from "../button";
 
 import classNames from "classnames";
+import ImageCard from "../image-card";
 
 interface Props {
   cssClasses?: string;
@@ -14,70 +15,16 @@ const OurRooms = ({ cssClasses }: Props) => {
   const [cardMove, setCardMove] = useState(false);
   return (
     <section
-      className={`grid gap-10 desktopSmall:grid-cols-[2fr_1fr] ${cssClasses}`}
+      className={`grid gap-8 desktopSmall:gap-10 desktopSmall:grid-cols-[2fr_1fr] ${cssClasses}`}
     >
-      <div className="grid gap-y-8">
-        {/* desktop view */}
-        <Link
-          href="/"
-          onMouseEnter={() => setCardMove(true)}
-          onMouseLeave={() => setCardMove(false)}
-          className={classNames(
-            "hidden tabletLarge:block absolute bg-white/[85%] py-10 desktopSmall:py-7 desktop:py-10 px-20 translate-y-[200px] -ml-12 desktopSmall:translate-y-36 desktop:translate-y-48 duration-300 ease-in-out",
-            {
-              "-ml-6": cardMove,
-            }
-          )}
-        >
-          <h2
-            className={classNames("text-[32px]", {
-              "text-brown": cardMove,
-            })}
-          >
-            Our Rooms
-          </h2>
-          <div className="flex gap-2">
-            <p className="uppercase text-[13px] text-pureBlack font-350 tracking-widest">
-              View More
-            </p>
-            <Image
-              src="/icons/plus.svg"
-              alt="Plus sign icon"
-              width={13}
-              height={13}
-              className="-translate-y-[1.5px] ease-in-out duration-500"
-            />
-          </div>
-        </Link>
-
-        {/* mobile view */}
-
-        <h2 className="tabletLarge:hidden">Our Rooms</h2>
-        <Image
-          src="https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/luna-blue/Home+page/Bedroom-2.jpg"
-          alt="Luna Blue rooms"
-          width={1000}
-          height={750}
-          className="h-[300px] phone:h-[325px] tablet:h-[425px] tabletLarge:h-[550px] desktopSmall:h-[425px] desktop:h-[550px] object-cover"
+      <div>
+        <h2 className="mb-8 tabletLarge:hidden">Our Rooms</h2>
+        <ImageCard
+          title="Our Homes"
+          image="bg-[url('https://the-wright-designs-website-images.s3.af-south-1.amazonaws.com/luna-blue/Home+page/Bedroom-2.jpg')]"
         />
-        <p className="tabletLarge:hidden">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt
-          commodi beatae recusandae reprehenderit maiores dicta tenetur ipsam
-          fugit, autem culpa ducimus perferendis natus necessitatibus quisquam
-          vel officia non laborum id totam dignissimos similique perspiciatis
-          veniam nesciunt. Obcaecati totam consectetur accusantium accusamus
-          labore minima excepturi. Asperiores dignissimos illum reprehenderit
-          molestiae explicabo voluptatem molestias voluptas sed, maiores qui
-          earum atque tempora repudiandae ab non, voluptatum unde maxime? Fugit
-          officiis assumenda minus odio optio maiores minima in quisquam?
-        </p>
-        <Button url="/" cssClasses="tabletLarge:hidden">
-          View More
-        </Button>
       </div>
-
-      {/* desktop view */}
-      <p className="hidden tabletLarge:block">
+      <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt
         commodi beatae recusandae reprehenderit maiores dicta tenetur ipsam
         fugit, autem culpa ducimus perferendis natus necessitatibus quisquam vel
@@ -88,6 +35,9 @@ const OurRooms = ({ cssClasses }: Props) => {
         tempora repudiandae ab non, voluptatum unde maxime? Fugit officiis
         assumenda minus odio optio maiores minima in quisquam?
       </p>
+      <Button url="/" cssClasses="tabletLarge:hidden">
+        View More
+      </Button>
     </section>
   );
 };
---
+++ components/image-card.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+import Button from "./button";
+
+import classNames from "classnames";
+
+interface Props {
+  cssClasses?: string;
+  title: string;
+  buttonText?: string | "View More";
+  image: string;
+  flip?: boolean;
+}
+
+const ImageCard = ({ cssClasses, title, buttonText, image, flip }: Props) => {
+  const [cardMove, setCardMove] = useState(false);
+
+  return (
+    <section
+      className={classNames(
+        `grid gap-10 grid-rows-3 ${image} bg-cover h-[300px] phone:h-[325px] tablet:h-[425px] tabletLarge:h-[550px] desktopSmall:h-[425px] desktop:h-[550px] place-items-start ${cssClasses}`,
+        {
+          "place-items-end ": flip,
+        }
+      )}
+    >
+      <div className="row-start-2">
+        {/* desktop view */}
+        <Link
+          href="/"
+          onMouseEnter={() => setCardMove(true)}
+          onMouseLeave={() => setCardMove(false)}
+          className={classNames(
+            "hidden tabletLarge:block bg-white/[85%] py-10 desktopSmall:py-7 desktop:py-10 pr-20 pl-10 duration-300 ease-in-out",
+            {
+              "pl-20 pr-10": flip,
+              "pl-14": cardMove,
+              "pr-14": cardMove && flip,
+            }
+          )}
+        >
+          <h2
+            className={classNames("text-[32px]", {
+              "text-brown": cardMove,
+            })}
+          >
+            {title}
+          </h2>
+          <div className="flex gap-2">
+            <p className="uppercase text-[13px] text-pureBlack font-350 tracking-widest">
+              {buttonText || "View More"}
+            </p>
+            <Image
+              src="/icons/plus.svg"
+              alt="Plus sign icon"
+              width={13}
+              height={13}
+              className="-translate-y-[1.5px] ease-in-out duration-500"
+            />
+          </div>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default ImageCard;

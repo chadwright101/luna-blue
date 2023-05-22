bc8e1bb243f6237e11bdef5adb0504a433eb2c3a
Chad Wright <justdeejays@gmail.com>
2023-05-22 20:26:00 +0200

    fix: add image fill property test

--- components/hero-split.tsx
+++ components/hero-split.tsx
@@ -11,14 +11,15 @@ interface Props {
 const HeroSplit = ({ url, cssClasses }: Props) => {
   const [hoverZoom, setHoverZoom] = useState(false);
   return (
-    <div className={`overflow-hidden ${cssClasses}`}>
+    <div
+      className={`overflow-hidden h-[275px] phone:h-[300px] tablet:h-[375px] tabletLarge:h-[425px] desktop:h-[500px] w-32 relative ${cssClasses}`}
+    >
       <Image
         src={url}
         alt="Luna Blue Guesthouse"
-        width={450}
-        height={500}
+        fill
         className={classNames(
-          "object-cover h-[275px] phone:h-[300px] tablet:h-[375px] tabletLarge:h-[425px] desktop:h-[500px] ease-in-out duration-500",
+          "object-cover w-full h-full ease-in-out duration-500",
           {
             "scale-[107.5%]": hoverZoom,
           }
@@ -26,7 +27,6 @@ const HeroSplit = ({ url, cssClasses }: Props) => {
         priority
         onMouseEnter={() => setHoverZoom(true)}
         onMouseLeave={() => setHoverZoom(false)}
-        sizes="(max-width: 425px) 60vw, (max-width: 900px) 50vw,(max-width: 1400px) 50vw, 25vw"
       />
     </div>
   );

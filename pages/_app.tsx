85e2a715bcd70778acbe89cfe1c7c4f61b4b7e08
Chad Wright <justdeejays@gmail.com>
2023-07-10 20:40:00 +0200

    feat: add window width component with global context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,17 +3,20 @@ import type { AppProps } from "next/app";
 import Footer from "@/components/navigation/footer/footer";
 import Header from "@/components/navigation/header/header";
 import Layout from "@/components/layout";
+import WindowWidthListener from "@/components/utils/window-width-listeners";
 
 import "@/styles/globals.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-      <Header cssClasses="sticky top-0 z-20 px-8 py-4 desktop:mb-8" />
-      <Layout>
-        <Component {...pageProps} />
-        <Footer cssClasses="mt-10" />
-      </Layout>
+      <WindowWidthListener>
+        <Header cssClasses="sticky top-0 z-20 px-8 py-4 desktop:mb-8" />
+        <Layout>
+          <Component {...pageProps} />
+          <Footer cssClasses="mt-10" />
+        </Layout>
+      </WindowWidthListener>
     </>
   );
 };

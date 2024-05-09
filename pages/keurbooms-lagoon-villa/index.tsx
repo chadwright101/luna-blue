e7366c6f9e7fa60d8b10f78643f363d156d44395
Chad Wright <justdeejays@gmail.com>
2024-05-09 11:18:38 +0200

    feat: add facebook pixel to project

--- pages/keurbooms-lagoon-villa/index.tsx
+++ pages/keurbooms-lagoon-villa/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import HomePage from "@/components/property-pages/home-page/home-page";
 import MetaComponent from "@/components/property-pages/meta-component";
 
@@ -6,6 +7,32 @@ import data from "@/data/lagoon-villa-data.json";
 export default function LagoonVilla() {
   return (
     <>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', 'EABsTCTbnx68BO7pal8hdZCDIYfaqwPM27cMzjPIY86JyeJ6ZA33JgmZBZAebxY4yktrbywubSKp1R6ZBmnQkFGqwqUqQDZAMQysKj2az3sQMcYDWhkdmZAGoUlIAdMKsXkSxGTRoKpAElMwWiKZCQrOFIqKFyueQZB7EwiJWa4gCBmPmkWejRcbZB5wpJNqT97n1oEMAZDZD');
+            fbq('track', 'PageView');
+          `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=EABsTCTbnx68BO7pal8hdZCDIYfaqwPM27cMzjPIY86JyeJ6ZA33JgmZBZAebxY4yktrbywubSKp1R6ZBmnQkFGqwqUqQDZAMQysKj2az3sQMcYDWhkdmZAGoUlIAdMKsXkSxGTRoKpAElMwWiKZCQrOFIqKFyueQZB7EwiJWa4gCBmPmkWejRcbZB5wpJNqT97n1oEMAZDZD&ev=PageView&noscript=1`}
+          />
+        </noscript>
+      </Head>
       <MetaComponent data={data} />
       <HomePage data={data} />
     </>

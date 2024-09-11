c8da11e6b6d4fde21904539d0e86a0d70abe2e3e
Chad Wright <justdeejays@gmail.com>
2024-09-11 11:07:08 +0200

    fix: update styling in policies component

--- components/property-pages/home-page/components/policies.tsx
+++ components/property-pages/home-page/components/policies.tsx
@@ -10,90 +10,20 @@ const Policies = () => {
 
   return (
     <div className="desktopSmall:col-span-2">
-      <h3 className="mb-8">
-        <Translated german="Richtlinien">Policies</Translated>
-      </h3>
       <div
         className={classNames("grid gap-8", {
           "desktopSmall:grid-cols-2":
             currentRoute === "/keurbooms-lagoon-villa",
         })}
       >
-        <ul className="grid gap-8">
-          <li
-            className={classNames("flex gap-2 flex-col", {
-              "desktopSmall:flex-row":
-                currentRoute !== "/keurbooms-lagoon-villa",
-            })}
-          >
-            <h4 className="text-18px font-Josefin_Sans font-500 underline underline-offset-4 decoration-[0.5px]">
-              <Translated german="Kautionsrichtlinie:">
-                Breakage deposit policy:
-              </Translated>
-            </h4>
-            <p>
-              {currentRoute === "/keurbooms-lagoon-villa" ? (
-                <Translated german="Bitte beachten Sie, dass für jeden Aufenthalt vor dem Check-in eine erstattbare Kaution in Höhe von R2500 zu hinterlegen ist.">
-                  Please note that a{" "}
-                  <span className="font-350 italic">
-                    refundable breakage deposit of R2500
-                  </span>{" "}
-                  is required to be paid for each stay before check-in.
-                </Translated>
-              ) : currentRoute === "/cliffside-suites" ? (
-                <Translated german="Bitte beachten Sie, dass für jeden Aufenthalt vor dem Check-in eine erstattbare Kaution in Höhe von R1000 zu hinterlegen ist.">
-                  Please note that a{" "}
-                  <span className="font-350 italic">
-                    refundable breakage deposit of R1000
-                  </span>{" "}
-                  is required to be paid for each stay before check-in.
-                </Translated>
-              ) : null}
-            </p>
-          </li>
-          <li
-            className={classNames("flex gap-2 flex-col", {
-              "desktopSmall:flex-row":
-                currentRoute !== "/keurbooms-lagoon-villa",
-            })}
-          >
-            <h4 className="text-18px font-Josefin_Sans font-500 underline underline-offset-4 decoration-[0.5px]">
-              <Translated german="Raucherrichtlinie:">
-                Smoking policy:
-              </Translated>
-            </h4>
-            <Translated german="Das Rauchen im Innenbereich ist strengstens untersagt, jedes Zimmer ist mit einem sensiblen Rauchmelder ausgestattet. Im Außenbereich stehen Aschenbecher zur Verfügung.">
-              Smoking indoors is strictly prohibited, each room is fitted with a
-              sensitive smoke detector. Ashtrays will be be provided outdoors.
-            </Translated>
-          </li>
-          <li
-            className={classNames("flex gap-2 flex-col", {
-              "desktopSmall:flex-row":
-                currentRoute !== "/keurbooms-lagoon-villa",
-            })}
-          >
-            <h4 className="text-18px font-Josefin_Sans font-500 underline underline-offset-4 decoration-[0.5px]">
-              <Translated german="Haustierrichtlinie:">Pets policy:</Translated>
-            </h4>
-            <Link
-              prefetch={false}
-              href="/assets/images/luna-blue-dog-policy-2023.pdf"
-              className="flex gap-2 hover:desktopSmall:text-brown p-3 text-blueLink -m-3 desktopSmall:p-0 desktopSmall:m-0"
-              target="_blank"
-            >
-              <Translated german="Herunterladen">Download</Translated>
-            </Link>
-          </li>
-        </ul>
         {currentRoute === "/keurbooms-lagoon-villa" && (
           <div className="grid gap-8 tabletLarge:grid-cols-[2fr_1fr] tabletLarge:gap-6 desktopSmall:grid-cols-1">
             <div>
-              <h4 className="mb-2 text-18px font-Josefin_Sans font-500 underline underline-offset-4 decoration-[0.5px]">
+              <h3 className="mb-8">
                 <Translated german="Nutzung der Outdoor-Ausrüstung">
                   Outdoor gear use
                 </Translated>
-              </h4>
+              </h3>
               <p>
                 <Translated german="Die folgende Outdoor-Ausrüstung steht den Gästen zur Verfügung (vorbehaltlich der Unterzeichnung eines Haftungsausschlusses).">
                   The following outdoor gear is available for guest use (subject
@@ -127,6 +57,81 @@ const Policies = () => {
             />
           </div>
         )}
+        <div>
+          <h3 className="mb-8">
+            <Translated german="Richtlinien">Policies</Translated>
+          </h3>
+          <ul className="grid gap-8">
+            <li
+              className={classNames("flex gap-2 flex-col", {
+                "desktopSmall:flex-row":
+                  currentRoute !== "/keurbooms-lagoon-villa",
+              })}
+            >
+              <h4 className="text-18px font-Josefin_Sans font-500 underline underline-offset-4 decoration-[0.5px]">
+                <Translated german="Kautionsrichtlinie:">
+                  Breakage deposit policy:
+                </Translated>
+              </h4>
+              <p>
+                {currentRoute === "/keurbooms-lagoon-villa" ? (
+                  <Translated german="Bitte beachten Sie, dass für jeden Aufenthalt vor dem Check-in eine erstattbare Kaution in Höhe von R2500 zu hinterlegen ist.">
+                    Please note that a{" "}
+                    <span className="font-350 italic">
+                      refundable breakage deposit of R2500
+                    </span>{" "}
+                    is required to be paid for each stay before check-in.
+                  </Translated>
+                ) : currentRoute === "/cliffside-suites" ? (
+                  <Translated german="Bitte beachten Sie, dass für jeden Aufenthalt vor dem Check-in eine erstattbare Kaution in Höhe von R1000 zu hinterlegen ist.">
+                    Please note that a{" "}
+                    <span className="font-350 italic">
+                      refundable breakage deposit of R1000
+                    </span>{" "}
+                    is required to be paid for each stay before check-in.
+                  </Translated>
+                ) : null}
+              </p>
+            </li>
+            <li
+              className={classNames("flex gap-2 flex-col", {
+                "desktopSmall:flex-row":
+                  currentRoute !== "/keurbooms-lagoon-villa",
+              })}
+            >
+              <h4 className="text-18px font-Josefin_Sans font-500 underline underline-offset-4 decoration-[0.5px]">
+                <Translated german="Raucherrichtlinie:">
+                  Smoking policy:
+                </Translated>
+              </h4>
+              <Translated german="Das Rauchen im Innenbereich ist strengstens untersagt, jedes Zimmer ist mit einem sensiblen Rauchmelder ausgestattet. Im Außenbereich stehen Aschenbecher zur Verfügung.">
+                Smoking indoors is strictly prohibited, each room is fitted with
+                a sensitive smoke detector. Ashtrays will be be provided
+                outdoors.
+              </Translated>
+            </li>
+            <li
+              className={classNames("flex gap-2 flex-col", {
+                "desktopSmall:flex-row":
+                  currentRoute !== "/keurbooms-lagoon-villa",
+              })}
+            >
+              <h4 className="text-18px font-Josefin_Sans font-500 underline underline-offset-4 decoration-[0.5px]">
+                <Translated german="Haustierrichtlinie:">
+                  Pets policy:
+                </Translated>
+              </h4>
+              <Link
+                prefetch={false}
+                href="/assets/images/luna-blue-dog-policy-2023.pdf"
+                className="flex gap-2 hover:desktopSmall:text-brown p-3 text-blueLink -m-3 desktopSmall:p-0 desktopSmall:m-0"
+                target="_blank"
+              >
+                <Translated german="Herunterladen">Download</Translated>
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
